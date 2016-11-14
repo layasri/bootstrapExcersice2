5b6ebbf4532389fa82ffa031024bf5e177235e8c
Layasri <layasri.subbarao@gmail.com>
2016-11-14 14:38:11 +0530

    Nov 14

---
+++ includes/js/script.js
@@ -0,0 +1,17 @@
+
+$(function(){
+
+  $('#alertMe').click(function(e){
+    e.preventDefault();
+    $('#successAlert').slideDown();
+  });
+
+  $('.bs-example-modal-lg').click(function(e) {
+    e.preventDefault();
+  });
+
+  // $('a.pop').popover();
+  //
+  // $('[rel="tooltip"]').tooltip();
+
+});
